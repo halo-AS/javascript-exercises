8cd0aa16e8e4b11442997b3a2090559109fc063c
halo-AS <208396210+halo-AS@users.noreply.github.com>
2025-08-13 00:50:57 -0400

    Do exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+
+    if(startNum > endNum){
+        const TEMP = startNum;
+        startNum = endNum;
+        endNum = TEMP;
+    }
+    if((!Number.isInteger(startNum) || !Number.isInteger(endNum)) || (startNum < 0 || endNum < 0)){
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    while(startNum <= endNum){
+        sum += startNum;
+        startNum++;
+    }
+
+    return sum;
 
 };
+
+console.log(sumAll(2, 8));
 
 // Do not edit below this line
 module.exports = sumAll;
