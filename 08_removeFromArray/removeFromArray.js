7ec16b763d1745a1117759d9f6a373d7961096fe
halo-AS <208396210+halo-AS@users.noreply.github.com>
2025-08-13 00:36:03 -0400

    Do exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arry) {
+
+    let args = [...arguments];
+    args.shift();
+    for(let i = 0; i < args.length; i++){
+        for(let j = 0; j < arry.length; j++){
+            if(arry[j] === args[i]){
+                arry.splice(j, 1);
+                j --;
+            }
+        }
+    }
+    return arry;
+
 };
+
+console.log(removeFromArray([1, 4, 8, 9, 9, "Hello", "Bye", 0], 9, "Bye", 4, 99));
 
 // Do not edit below this line
 module.exports = removeFromArray;
