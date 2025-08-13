bd8ec44f60543ddd3d9bda34837020cd5dbc0b0d
halo-AS <208396210+halo-AS@users.noreply.github.com>
2025-08-12 23:46:45 -0400

    Do the exercises

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arrayStr = Array.from(str);
+    let backwardsStr = "";
+    for(let i = (arrayStr.length - 1); i > -1; i--){
+        backwardsStr += arrayStr[i];
+    }
+    return backwardsStr;
 };
+
+console.log(reverseString("Hello World"))
 
 // Do not edit below this line
 module.exports = reverseString;
