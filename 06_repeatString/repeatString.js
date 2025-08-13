36172ec21ec7f6f77e0d642c213cee649983c98e
halo-AS <208396210+halo-AS@users.noreply.github.com>
2025-08-12 23:37:25 -0400

    Do the exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if(count < 0){
+        return "ERROR";
+    }
+    let repeatedStr = "";
+    for(let i = 0; i < count; i++){
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
