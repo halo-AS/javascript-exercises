3c97fc5e0298b5d992c58a3df48e6fcb76959975
halo-AS <208396210+halo-AS@users.noreply.github.com>
2025-08-13 01:08:42 -0400

    Do exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFar) {
+
+  return Math.round(((tempFar - 32) * (5/9)) * 10) / 10;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+
+  return Math.round((tempCel * (9/5) + 32) * 10) / 10;
+
 };
 
 // Do not edit below this line
